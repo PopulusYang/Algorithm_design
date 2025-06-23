b3ee7108a62c9f22f0aac1e2b3d59f55e1e98027
Populus <ywxzgr2005@163.com>
2025-06-23 10:57:23 +0800

    创建任务3

---
+++ collect_coin.cpp
@@ -0,0 +1,13 @@
+#include "collect_coin.h"
+
+std::vector<point> coin_collecter::get_path(MAZE **newinformation = nullptr)
+{
+    point b;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            wait();//暂时放这里
+        }
+    }
+}
