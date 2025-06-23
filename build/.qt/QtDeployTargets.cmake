5ccba62dcb7ebe5824960c1f0d67d92f222d7154
doloris <2756141076@qq.com>
2025-06-23 08:57:46 +0800

    配置qt环境

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_CPTDesign_FILE D:/vscode_algo/build/CPTDesign.exe)
+set(__QT_DEPLOY_TARGET_CPTDesign_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_CPTDesign_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
---
+++ build/CMakeFiles/CPTDesign_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPTDesign_autogen"
+  "CPTDesign_autogen/include/ui_mainwindow.h"
+  "CPTDesign_autogen/mocs_compilation.cpp"
+  "CPTDesign_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/CPTDesign_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
