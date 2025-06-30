77769f5e5c0f052aae8808ff6041abb5ac49b5ba
Populus <ywxzgr2005@163.com>
2025-06-30 11:49:48 +0800

    ？

--- gamemain.h
+++ gamemain.h
@@ -116,7 +116,7 @@ public:
 
     bool inBounds(int x, int y)//判断是否越界
     {
-        return x > 0 && y > 0 && x < mazesize && y < mazesize;
+        return x >= 0 && y >= 0 && x <= mazesize && y <= mazesize;
     }
     int maze[MAXSIZE][MAXSIZE];
     int dimension;
